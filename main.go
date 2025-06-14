28e0cd7aa172018faff5ca56a4fb61d4a7c788a5
Rodion Zherdev <r.zherdev@vigbo.com>
2025-06-14 12:40:44 +0400

    feat: Добавляет начальную логику конвертера

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Bin struct {
+	id        int
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+type BinList struct {
+	binList []Bin
+}
+
+func NewBin(id int, private bool, name string) *Bin {
+	return &Bin{
+		id:        id,
+		private:   private,
+		createdAt: time.Now(),
+		name:      name,
+	}
+}
+
+func (bl *BinList) AddBin(bin *Bin) {
+	bl.binList = append(bl.binList, *bin)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	binList := BinList{}
+
+	bin := NewBin(1, true, "test")
+	binList.AddBin(bin)
+
+	fmt.Printf("%+v\n", binList)
 }
